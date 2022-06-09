d8e7f9106a99a7afc19d7c4e4fafb98cb5f564e9
Kholodov Sem <kholodovsem@gmail.com>
2022-06-09 15:07:36 +0300

    Update

--- src/JS/modal-form.js
+++ src/JS/modal-form.js
@@ -3,14 +3,15 @@
     openModalBtn: document.querySelector('[data-modal-open_form]'),
     closeModalBtn: document.querySelector('.modal__button--close--form'),
     modal: document.querySelector('[data-modal_form]'),
-    body: document.querySelector("body"),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMobileModalButton.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden_form');
-    refs.body.classList.toggle("no-scroll");
+    refs.body.classList.toggle('no-scroll');
   }
 })();
