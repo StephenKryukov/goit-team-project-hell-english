30d20ade8b7adb1e557be7e371351ebf651e793f
Kholodov Sem <kholodovsem@gmail.com>
2022-06-09 15:30:57 +0300

    1

--- src/JS/up-button.js
+++ src/JS/up-button.js
@@ -25,16 +25,3 @@
   window.addEventListener('scroll', trackScroll);
   goTopBtn.addEventListener('click', backToTop);
 })();
-
-const anchors = document.querySelectorAll('a[href*="#"]');
-
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (event) {
-    event.preventDefault();
-    const blockID = anchor.getAttribute('href');
-    document.querySelector('' + blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  });
-}
